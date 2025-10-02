74e6f8f27566edac1c7f75a1a13e8a266bdb2b0e
chenmzh <mingzhchen@ethz.ch>
2025-10-02 16:38:49 +0200

    Complete (sub)Experiment date 2025-10-02 and time 04:38 PM, with experiment message: Testing 5m / 30xK second PWM pattenring with 20m pauses

--- Yeast_source/matlab_files/exp_config.m
+++ Yeast_source/matlab_files/exp_config.m
@@ -1,18 +1,55 @@
 function [config_exp] = exp_config(imagingFolderName)
 
+
+    % PWM pattent setup
+
+    % Progressive PWM pattern with increasing cycle periods
+    cycle_periods = [30, 60, 90, 120, 150, 180, 210, 240, 270, 300]; % seconds
+    pwm_duration = 300; % 5 minutes
+    off_duration = 1200; % 20 minutes
+
+    times = [];
+    values = [];
+
+    for i = 1:length(cycle_periods)
+        % Add 20 min off period
+        times = [times, off_duration];
+        values = [values, 0];
+        
+        cycle_period = cycle_periods(i);
+        
+        if cycle_period == 300
+            % Constant light for 5 minutes
+            times = [times, 300];
+            values = [values, 1];
+        else
+            % Generate PWM with 50% duty cycle
+            [pwm_times, pwm_values] = pwm_pattern(pwm_duration, cycle_period/2, cycle_period/2);
+            times = [times, pwm_times];
+            values = [values, pwm_values];
+        end
+    end
+
+    % Final 20 min off period
+    times = [times, off_duration];
+    values = [values, 0];
+
+    times_pwm_pattern = times;
+    values_pwm_pattern = values;
+
     %% CONFIGURATION PARAMETERS
     config_exp = [];
     %% EXPERIMENT SPECIFIC PARAMETERS
     config_exp.experiment_name = 'Yeast_Git';
-    config_exp.time_date = '20251001';
-    config_exp.time_hour = '123021';
+    config_exp.time_date = '20251002';
+    config_exp.time_hour = '112317';
     config_exp.organism = 'Yeast';
     config_exp.objective_type = '40x_oil';
     config_exp.magnification = '40x*1.5=60x';
-    config_exp.strains = 'GE';
-    config_exp.initial_delay = 60*20; %% In seconds
-    config_exp.experiment_pattern_times = repmat([1200,120,120,120,240,120,480,120,920,120,1800,120,3200],1,1);	
-    config_exp.experiment_pattern_values = repmat([0,1,0,1,0,1,0,1,0,1,0,1,0],1,1);
+    config_exp.strains = 'BGE';
+    config_exp.initial_delay = 60*10; %% In seconds
+    config_exp.experiment_pattern_times = times_pwm_pattern;	
+    config_exp.experiment_pattern_values = values_pwm_pattern;
     config_exp.Period = 60;
     config_exp.intensity = 17.3;
     config_exp.light_normalization = 69.2;
@@ -21,6 +58,6 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.imaging.exposure = {10, 2000};
     config_exp.imaging.zOffsets = {[0], [0]};
     config_exp.imaging.condenser = {5, 5};
-    config_exp.imaging.message = ['Testing patterns with short bursts and ovelapping peaks, fast sampling 1 img/min, longer light pattern (120s), GE strain'];
+    config_exp.imaging.message = ['Testing 5m / 30xK second PWM pattenring with 20m pauses'];
     config_exp.UsingPFS = true;
 end
