63f9978420be4e1090a8634df0a824a8d4c02a29
chenmzh <mingzhchen@ethz.ch>
2025-10-03 17:36:02 +0200

    Complete (sub)Experiment date 2025-10-03 and time 05:35 PM, with experiment message:

--- Yeast_source/matlab_files/exp_config.m
+++ Yeast_source/matlab_files/exp_config.m
@@ -4,9 +4,16 @@ function [config_exp] = exp_config(imagingFolderName)
     % PWM pattent setup
 
     % Progressive PWM pattern with increasing cycle periods
-    cycle_periods = [30, 60, 90, 120, 150, 180, 210, 240, 270, 300]; % seconds
-    pwm_duration = 300; % 5 minutes
+    
+    cycle_periods = [30, 60, 120, 180, 240, 300]; % seconds
+    % cycle_on_time = [30, 60, 120, 180, 240, 300, 360, 600]; % seconds
+    
+    pwm_duration = 1800; % 30 minutes
     off_duration = 1200; % 20 minutes
+    % Pulsatile Period
+    % PP = 300 % 5 mins
+    % Frequency = pwm_duration/PP
+    % On_time = cycle_periods/Frequency
 
     times = [];
     values = [];
@@ -18,16 +25,12 @@ function [config_exp] = exp_config(imagingFolderName)
         
         cycle_period = cycle_periods(i);
         
-        if cycle_period == 300
-            % Constant light for 5 minutes
-            times = [times, 300];
-            values = [values, 1];
-        else
-            % Generate PWM with 50% duty cycle
-            [pwm_times, pwm_values] = pwm_pattern(pwm_duration, cycle_period/2, cycle_period/2);
-            times = [times, pwm_times];
-            values = [values, pwm_values];
-        end
+        % Generate PWM with 50% duty cycle
+        [pwm_times, pwm_values] = pwm_pattern(pwm_duration, cycle_period/2, cycle_period/2);
+        % [pwm_times, pwm_values] = pwm_pattern(pwm_duration, On_time, PP - On_time);
+        times = [times, pwm_times];
+        values = [values, pwm_values];
+
     end
 
     % Final 20 min off period
@@ -41,8 +44,8 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp = [];
     %% EXPERIMENT SPECIFIC PARAMETERS
     config_exp.experiment_name = 'Yeast_Git';
-    config_exp.time_date = '20251002';
-    config_exp.time_hour = '112317';
+    config_exp.time_date = '20251003';
+    config_exp.time_hour = '115203';
     config_exp.organism = 'Yeast';
     config_exp.objective_type = '40x_oil';
     config_exp.magnification = '40x*1.5=60x';
@@ -50,7 +53,7 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.initial_delay = 60*10; %% In seconds
     config_exp.experiment_pattern_times = times_pwm_pattern;	
     config_exp.experiment_pattern_values = values_pwm_pattern;
-    config_exp.Period = 60;
+    config_exp.Period = 120;
     config_exp.intensity = 17.3;
     config_exp.light_normalization = 69.2;
     config_exp.imaging.types = {'brightfield','Cy3'};
@@ -58,6 +61,6 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.imaging.exposure = {10, 2000};
     config_exp.imaging.zOffsets = {[0], [0]};
     config_exp.imaging.condenser = {5, 5};
-    config_exp.imaging.message = ['Testing 5m / 30xK second PWM pattenring with 20m pauses'];
+    config_exp.imaging.message = ['Testing 20m / x second PWM pattenring with 10m pauses'];
     config_exp.UsingPFS = true;
 end
