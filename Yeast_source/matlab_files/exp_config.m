5e04562ad27d98c2c499a083b01380b8694d2d30
chenmzh <mingzhchen@ethz.ch>
2025-10-13 11:16:48 +0200

    Create Experiment date 20251013 and time 111648, having a fresh start

--- Yeast_source/matlab_files/exp_config.m
+++ Yeast_source/matlab_files/exp_config.m
@@ -59,11 +59,11 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.Period = 60;
     config_exp.intensity = 17.3;
     config_exp.light_normalization = 69.2;
-    config_exp.imaging.types = {'brightfield','Cy3'};
-    config_exp.imaging.groups = {'Channels','Trigger'};
-    config_exp.imaging.exposure = {10, 2000};
-    config_exp.imaging.zOffsets = {[0], [0]};
-    config_exp.imaging.condenser = {5, 5};
-    config_exp.imaging.message = ['Test DMD pattern. Background 1.6uW, 2048*0.07um/pixel = 143.36um, 37.6uW - 1.6 = 36uW before, = 0.00175uW/um^2 = 175mW/cm^2, with 50/255, power is 8.73uW, 8.73-1.6 = 7.13uW. normalized to be 34.7mW/cm^2, keep lowering it by 3.4 times. targeting light value is 10mW/cm^2, which is roughly 2uW, add background 1.6 which is 3.6uW. In conclustion, its lowered by 4 times compared with previous setup. Using patterns C:\Users\Localadmin\Desktop\sant_workspace\sample_patterns\checkerboard_fullHD_int8_0_50_1920x1080.png, tested with high duty cycle'];
+    config_exp.imaging.types = {'brightfield','Cy3','projector'};
+    config_exp.imaging.groups = {'Channels','Trigger','Channels'};
+    config_exp.imaging.exposure = {10, 2000, 10};
+    config_exp.imaging.zOffsets = {[0], [0], [0]};
+    config_exp.imaging.condenser = {5, 5, 5};
+    config_exp.imaging.message = ['Test DMD pattern. with capturing the DMD images. Background 1.6uW, 2048*0.07um/pixel = 143.36um, 37.6uW - 1.6 = 36uW before, = 0.00175uW/um^2 = 175mW/cm^2, with 50/255, power is 8.73uW, 8.73-1.6 = 7.13uW. normalized to be 34.7mW/cm^2, keep lowering it by 3.4 times. targeting light value is 10mW/cm^2, which is roughly 2uW, add background 1.6 which is 3.6uW. In conclustion, its lowered by 4 times compared with previous setup. Using patterns C:\Users\Localadmin\Desktop\sant_workspace\sample_patterns\checkerboard_fullHD_int8_0_50_1920x1080.png, tested with high duty cycle'];
     config_exp.UsingPFS = true;
 end
